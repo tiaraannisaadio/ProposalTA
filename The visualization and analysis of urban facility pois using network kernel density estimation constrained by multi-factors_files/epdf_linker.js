594ef2a986bbdd160c6dbef41dc18c1b2b431f73
unknown <tiarannisaa@gmail.com>
2016-03-30 07:44:30 +0700

    ta la la laa

---
+++ The visualization and analysis of urban facility pois using network kernel density estimation constrained by multi-factors_files/epdf_linker.js
@@ -0,0 +1,10 @@
+(function() {
+  try {
+    window['readcube-request-ua'] = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36';
+  } catch(e) {}
+  var script = document.createElement('script');
+  script.async = true;
+  script.type = 'text/javascript';
+  script.src = 'https://content-assets.readcube.com/assets/scielo_epdf_linker-331ab7564107a002811615aca957d554.js';
+  document.getElementsByTagName('head').item(0).appendChild(script);
+})();
